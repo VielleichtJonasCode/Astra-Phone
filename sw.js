2eea06628148762c428cb2343753a5b6936c968a
VielleichtJonasCode <czfvmhzd2k@privaterelay.appleid.com>
2026-09-08 12:54:37 +0200

    Add files via upload
    bug fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-/* Offline-Cache für die Signier-Web-App. */
-const CACHE = 'astra-sign-v1'
-const ASSETS = ['./', './index.html', './manifest.webmanifest']
+/* Offline-Cache für die Signier-Web-App. Netzwerk zuerst, damit Updates ankommen. */
+const CACHE = 'astra-sign-v3'
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './sw.js']
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()))
@@ -8,25 +8,24 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
   )
 })
 
 self.addEventListener('fetch', (e) => {
   if (e.request.method !== 'GET') return
   e.respondWith(
-    caches.match(e.request, { ignoreSearch: true }).then(
-      (hit) =>
-        hit ||
-        fetch(e.request)
-          .then((res) => {
-            const copy = res.clone()
-            caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {})
-            return res
-          })
-          .catch(() => caches.match('./index.html'))
-    )
+    fetch(e.request)
+      .then((res) => {
+        const copy = res.clone()
+        caches.open(CACHE).then((c) => c.put(e.request, copy)).catch(() => {})
+        return res
+      })
+      .catch(() =>
+        caches.match(e.request, { ignoreSearch: true }).then((hit) => hit || caches.match('./index.html'))
+      )
   )
 })
